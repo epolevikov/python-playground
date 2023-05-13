1f1a744116bc96c672e44803d112fbc6206f49bd
evgeny <polevikovea@gmail.com>
2023-05-13 04:29:04 +0300

    Add async sample

---
+++ async_sample.py
@@ -0,0 +1,22 @@
+import asyncio
+import time
+
+async def say_after(delay, what):
+    await asyncio.sleep(delay)
+    print(what)
+
+
+async def main():
+    task1 = asyncio.create_task(say_after(1, "Hello"))
+    task2 = asyncio.create_task(say_after(2, "World"))
+
+    print(f"started at {time.strftime('%X')}")
+
+    await task1
+    await task2
+
+    # Should take about 2 seconds
+    print(f"finished at {time.strftime('%X')}")
+
+
+asyncio.run(main())
